72ad2a9a4269583630d6c8bc337e53ca684a58e1
NickLegacy <nicklegacy13@gmail.com>
2022-09-27 14:52:25 -0400

    initial commit

---
+++ Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Static
+{
+    static class TempConverter
+    {
+        public static double FahrenheitToCelsius (double Fahrenheit)
+        {
+            var result = (Fahrenheit - 32) / 1.8;
+            return result;
+        }
+
+        public static double CelsiusToFahrenheit (double Celsius)
+        {
+            var result = (Celsius * 1.8) + 32;
+            return result;
+        }
+
+
+
+
+
+    }
+}
